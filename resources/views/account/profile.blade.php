e2d7354470c0292278de2487b45fae5e5aed3a3f
syaafiudinm <altafpasallo12@gmail.com>
2024-07-24 20:22:15 +0800

    bug fixing in profile section

--- resources/views/account/profile.blade.php
+++ resources/views/account/profile.blade.php
@@ -25,26 +25,7 @@
                     Navigation
                 </div>
                 <div class="card-body sidebar">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a href="book-listing.html">Books</a>                               
-                        </li>
-                        <li class="nav-item">
-                            <a href="reviews.html">Reviews</a>                               
-                        </li>
-                        <li class="nav-item">
-                            <a href="profile.html">Profile</a>                               
-                        </li>
-                        <li class="nav-item">
-                            <a href="my-reviews.html">My Reviews</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="change-password.html">Change Password</a>
-                        </li> 
-                        <li class="nav-item">
-                            <a href="{{route('account.logout')}}">Logout</a>
-                        </li>                           
-                    </ul>
+                    @include('layouts.sidebar')
                 </div>
             </div>
         </div>
